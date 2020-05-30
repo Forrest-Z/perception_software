ffa089f3551b774b39316b952fc9d3f5dcd5d7be
lipeijie <lipeijie1@geely.com>
2020-05-30 14:40:27 +0800

    lipeijie add imgproc_c

---
+++ ai_modules/cmake/ai_dependencies.cmake
@@ -0,0 +1,6 @@
+set(OpenCV_DIR /home/lpj/Software/opencv34/share/OpenCV)
+find_package(OpenCV REQUIRED CONFIG)
+message(STATUS "Found OpenCV ${OpenCV_VERSION}")
+message(STATUS "${OpenCV_INCLUDE_DIRS}, ${OpenCV_LIBRARIES}")
+include_directories(${OpenCV_INCLUDE_DIRS})
+link_directories(${OpenCV_LIBRARIES})
