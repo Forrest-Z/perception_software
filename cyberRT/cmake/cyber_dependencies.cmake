b71b9d122e1558afc6c1cfacfac971de7eee7d6f
lipeijie <lipeijie1@geely.com>
2020-09-21 16:47:47 +0800

    lipeijie update cyberRT

--- cyberRT/cmake/cyber_dependencies.cmake
+++ cyberRT/cmake/cyber_dependencies.cmake
@@ -1,25 +1,29 @@
-set(fastrtps_DIR ${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/fastrtps/lib/fastrtps/cmake)
+#include cyberRT_project directories
+include_directories(${cyberRT_project_BINARY_DIR})
+include_directories(${cyberRT_project_SOURCE_DIR})
+
+set(fastrtps_DIR ${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/fastrtps/lib/fastrtps/cmake)
 find_package(fastrtps REQUIRED CONFIG)
 message(STATUS "Found fastrtps ${fastrtps_VERSION}")
 message(STATUS "${fastrtps_LIB_DIR}, ${fastrtps_INCLUDE_DIR}")
 include_directories(${fastrtps_INCLUDE_DIR})
 link_directories(${fastrtps_LIB_DIR})
 
-set(fastcdr_DIR ${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/fastcdr/lib/fastcdr/cmake)
+set(fastcdr_DIR ${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/fastcdr/lib/fastcdr/cmake)
 find_package(fastcdr REQUIRED CONFIG)
 message(STATUS "Found fastcdr ${fastcdr_VERSION}")
 message(STATUS "${fastcdr_LIB_DIR}, ${fastcdr_INCLUDE_DIR}")
 include_directories(${fastcdr_INCLUDE_DIR})
 link_directories(${fastcdr_LIB_DIR})
 
-#set(Protobuf_ROOT ${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf)
-#set(Protobuf_DIR ${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf/lib/cmake/protobuf)
+#set(Protobuf_ROOT ${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf)
+#set(Protobuf_DIR ${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf/lib/cmake/protobuf)
 #option(protobuf_VERBOSE "Enable for verbose output" ON)
 #option(protobuf_MODULE_COMPATIBLE "CMake build-in FindProtobuf.cmake module compatible" ON)
 set(Protobuf_PREFIX_PATH
-    "${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf/include"
-    "${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf/lib"
-    "${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf/bin"
+    "${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf/include"
+    "${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf/lib"
+    "${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/protobuf/bin"
 )
 list(APPEND CMAKE_PREFIX_PATH "${Protobuf_PREFIX_PATH}")
 find_package(Protobuf REQUIRED NOCONFIG)
@@ -29,7 +33,7 @@ include_directories(${Protobuf_INCLUDE_DIRS})
 link_directories(${Protobuf_LIBRARIES})
 
 
-set(Poco_DIR ${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/Poco/lib/cmake/Poco)
+set(Poco_DIR ${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/Poco/lib/cmake/Poco)
 find_package(Poco REQUIRED COMPONENTS Foundation CONFIG)
 message(STATUS "version: ${Poco_VERSION}")
 message(STATUS "Found Poco: ${Poco_LIBRARIES}")
@@ -41,20 +45,20 @@ message(STATUS "Found Poco: ${Poco_LIBRARIES}")
 #include_directories(/home/allen/aarch64-depends/tinyxml2/include)
 #link_directories(/home/allen/aarch64-depends/tinyxml2/lib)
 
-#set(gflags_DIR ${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gflags/lib/cmake/gflags)
+#set(gflags_DIR ${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gflags/lib/cmake/gflags)
 #find_package(gflags REQUIRED CONFIG)
 #message(STATUS "version: ${gflags_VERSION}")
 #message(STATUS "Found gflags: ${gflags_LIBRARIES}")
-include_directories(${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gflags/include)
-link_directories(${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gflags/lib)
+include_directories(${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gflags/include)
+link_directories(${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gflags/lib)
 
 
-#set(glog_DIR ${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/glog/lib/cmake/glog)
+#set(glog_DIR ${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/glog/lib/cmake/glog)
 #find_package(glog REQUIRED CONFIG)
 #message(STATUS "version: ${glog_VERSION}")
 #message(STATUS "Found glog: ${glog_LIBRARIES}")
-include_directories(${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/glog/include)
-link_directories(${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/glog/lib)
+include_directories(${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/glog/include)
+link_directories(${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/glog/lib)
 
-include_directories(${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gtest/include)
-link_directories(${cyber_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gtest/lib)
+include_directories(${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gtest/include)
+link_directories(${cyberRT_project_SOURCE_DIR}/third_party/${CMAKE_SYSTEM_PROCESSOR}/gtest/lib)
--- cyberRT/cyber/setup.bash
+++ cyberRT/cyber/setup.bash
@@ -5,11 +5,11 @@ third_path="${CYBER_PATH}/third_party"
 cyber_tool_path="${binary_path}/cyber/tools"
 recorder_path="${cyber_tool_path}/cyber_recorder"
 monitor_path="${cyber_tool_path}/cyber_monitor"
-PYTHON_LD_PATH="${library_path}/cyber/cyber_py3"
-launch_path="${CYBER_PATH}/tools/cyber_launch"
-channel_path="${CYBER_PATH}/tools/cyber_channel"
-node_path="${CYBER_PATH}/tools/cyber_node"
+launch_path="${cyber_tool_path}/cyber_launch"
+channel_path="${cyber_tool_path}/cyber_channel"
+node_path="${cyber_tool_path}/cyber_node"
 service_path="${CYBER_PATH}/tools/cyber_service"
+PYTHON_LD_PATH="${library_path}/cyber/cyber_py3"
 #qt_path=/usr/local/Qt5.5.1/5.5/gcc_64
 
 #protobuf
@@ -45,7 +45,7 @@ export PYTHONPATH=${PYTHON_LD_PATH}:${library_path}/python:$PYTHONPATH
 export CYBER_DOMAIN_ID=80
 export CYBER_IP=127.0.0.1
 
-export GLOG_log_dir=/home/$USER/cyber_data/data/log
+export GLOG_log_dir=/home/$USER/cyber_data/log
 export GLOG_alsologtostderr=0
 export GLOG_colorlogtostderr=1
 export GLOG_minloglevel=0
